e452084538203fa9400846812f469ab9a37b61d5
abdoulrasheed <abdoulrasheedibrahim47@gmail.com>
2021-08-14 22:37:49 +0100

    Update the project to add items on button click

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 import style from "../static/css/Home.module.css";
 
 const Home = () => {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const onChange = (e) => {
+    setTask(`${task}${e.nativeEvent.data}`); // back ticks string concatenation ES6
+  };
+
+  const onAdd = () => {
+    const newTask = { name: task };
+    const newTasks = [...tasks, newTask]; // spread operator
+    setTasks(newTasks);
+    setTask("");
+  };
+
   return (
     <div className={style.container}>
       <div className={style.inputContainer}>
-        <input type="text" placeholder="Enter a Task" className={style.input} />
-        <button className={style.primaryButton}>Add</button>
+        <input
+          type="text"
+          placeholder="Enter a Task"
+          className={style.input}
+          onChange={onChange} // call by reference
+          value={task}
+        />
+        <button className={style.primaryButton} onClick={onAdd}>
+          Add
+        </button>
       </div>
 
       <ul className={style.taskList}>
-        <li>Task 1</li>
-        <li>Task 2</li>
-        <li>Task 3</li>
-        <li>Task 4</li>
-        <li>Task 5</li>
-        <li>Task 6</li>
+        {tasks.map(
+          (
+            item // map,reduce functions
+          ) => (
+            <li>{item.name}</li>
+          )
+        )}
+        {/* <li>please create task</li> */}
       </ul>
     </div>
   );
